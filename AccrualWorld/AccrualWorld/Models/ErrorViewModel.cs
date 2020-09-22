03fcffa91e5566c88e12c70afcf2e5629b1e4f8c
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-09-22 14:45:10 -0400

    initial setup without database

---
+++ AccrualWorld/AccrualWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace AccrualWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId
+        {
+            get; set;
+        }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
