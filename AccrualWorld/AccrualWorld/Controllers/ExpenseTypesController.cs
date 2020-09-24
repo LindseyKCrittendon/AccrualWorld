dd53424c968945dd376dcf78127e7c03e3d6d9ea
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-09-24 13:35:27 -0400

    added expense type controller and included expenses in the index

---
+++ AccrualWorld/AccrualWorld/Controllers/ExpenseTypesController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AccrualWorld.Data;
+using AccrualWorld.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace AccrualWorld.Controllers
+{
+    public class ExpenseTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        
+
+        public ExpenseTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+            
+        }
+
+        
+
+        // GET: ExpenseTypes
+        public async Task<IActionResult> Index()
+        {
+
+            var ExpenseType = await _context.ExpenseTypes
+                .Include(e => e.Expenses)
+                .ToListAsync();
+
+            return View(ExpenseType);
+            //return View(await _context.ExpenseTypes.ToListAsync());
+        }
+
+        // GET: ExpenseTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expenseType = await _context.ExpenseTypes
+                .FirstOrDefaultAsync(m => m.ExpenseTypeId == id);
+            if (expenseType == null)
+            {
+                return NotFound();
+            }
+
+            return View(expenseType);
+        }
+
+        // GET: ExpenseTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ExpenseTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ExpenseTypeId,Label")] ExpenseType expenseType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(expenseType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(expenseType);
+        }
+
+        // GET: ExpenseTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expenseType = await _context.ExpenseTypes.FindAsync(id);
+            if (expenseType == null)
+            {
+                return NotFound();
+            }
+            return View(expenseType);
+        }
+
+        // POST: ExpenseTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ExpenseTypeId,Label")] ExpenseType expenseType)
+        {
+            if (id != expenseType.ExpenseTypeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(expenseType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExpenseTypeExists(expenseType.ExpenseTypeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(expenseType);
+        }
+
+        // GET: ExpenseTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expenseType = await _context.ExpenseTypes
+                .FirstOrDefaultAsync(m => m.ExpenseTypeId == id);
+            if (expenseType == null)
+            {
+                return NotFound();
+            }
+
+            return View(expenseType);
+        }
+
+        // POST: ExpenseTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var expenseType = await _context.ExpenseTypes.FindAsync(id);
+            _context.ExpenseTypes.Remove(expenseType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ExpenseTypeExists(int id)
+        {
+            return _context.ExpenseTypes.Any(e => e.ExpenseTypeId == id);
+        }
+    }
+}
