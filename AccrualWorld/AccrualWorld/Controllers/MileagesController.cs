6b95e8ca0fb90490fd7d93ee8ec9409faec6d4e9
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-09-27 01:56:02 -0400

    fixed edit, delete, details

--- AccrualWorld/AccrualWorld/Controllers/MileagesController.cs
+++ AccrualWorld/AccrualWorld/Controllers/MileagesController.cs
@@ -175,6 +175,8 @@ namespace AccrualWorld.Controllers
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var mileage = await _context.Mileages.FindAsync(id);
+            var user = await GetCurrentUserAsync();
+            mileage.UserId = user.Id;
             _context.Mileages.Remove(mileage);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
