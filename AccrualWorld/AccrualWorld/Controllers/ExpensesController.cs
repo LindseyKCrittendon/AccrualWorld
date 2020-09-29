f8927ad708c83331adfb970ac8bce28434ab3c7e
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-09-29 15:37:16 -0400

    printing all receipts but can filter by date range

--- AccrualWorld/AccrualWorld/Controllers/ExpensesController.cs
+++ AccrualWorld/AccrualWorld/Controllers/ExpensesController.cs
@@ -283,16 +283,24 @@ namespace AccrualWorld.Controllers
         }
 
         // GET: Receipts
-        public async Task<IActionResult> Receipt()
+        public async Task<IActionResult> Receipt(DateTime? start, DateTime? end)
         {
             //Added user information to only show information for the correct user
             ApplicationUser loggedInUser = await GetCurrentUserAsync();
 
+            //trying to get date range functionality
             var expense = await _context.Expenses
                 .Include(et => et.ExpenseType)
                 .Include(u => u.User)
-                .Where(expense => expense.UserId == loggedInUser.Id)
+                .Where(expense => expense.UserId == loggedInUser.Id && (!start.HasValue || expense.DateTime >= start) && (!end.HasValue || expense.DateTime <= end))
                 .ToListAsync();
+
+
+           // var expense = await _context.Expenses
+           //     .Include(et => et.ExpenseType)
+           //    .Include(u => u.User)
+           //    .Where(expense => expense.UserId == loggedInUser.Id)
+           //   .ToListAsync();
             return View(expense);
 
             
