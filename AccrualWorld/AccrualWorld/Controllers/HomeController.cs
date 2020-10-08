5a866e949c8a2fd41526400826834fcfc08244af
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-10-08 14:58:01 -0400

    incomes showing up correctly in graph

--- AccrualWorld/AccrualWorld/Controllers/HomeController.cs
+++ AccrualWorld/AccrualWorld/Controllers/HomeController.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AccrualWorld.Models;
+using AccrualWorld.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using AccrualWorld.Models.DashboardViewModels;
 
 namespace AccrualWorld.Controllers
 {
@@ -13,14 +17,55 @@ namespace AccrualWorld.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _context;
+
+        // Add private field to hold our user manager
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            _context = context;
+            _userManager = userManager;
         }
+
+        // Get the currently logged in user
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            
+            //ApplicationUser loggedInUser = await GetCurrentUserAsync();
+            //INSTANCE OF VIEW MODEL
+             DashboardViewModel ViewModel = new DashboardViewModel();
+            //TODO:: DATE RANGE PICKER FOR GRAPHS AND CHARTS
+            var incomes = _context.Incomes.ToList();
+            //   .Include(m => m.User)
+            //adds conditional information for date range picker.
+            //  .Where(income => income.UserId == loggedInUser.Id)
+            //  .ToListAsync();
+            // return View();
+            List<double> totals = new List<double>();
+            for (int p = 0; p < 12; p++)
+            {
+                var number = 0.0;
+                for (int i = 0; i < incomes.Count; i++)
+                {
+                    number += incomes.Where(t => t.DateTime.Month == (p + 1)).Sum(t => t.Total);
+                }
+                totals.Add(number);
+            }
+
+          
+            ViewModel.monthlyTotals = totals;
+            ViewData["totals"] = totals.ToArray();
+            return View(ViewModel);
+            
         }
 
         public IActionResult Privacy()
